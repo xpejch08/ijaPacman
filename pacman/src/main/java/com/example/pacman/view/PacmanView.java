099a715fdb028f55b6d11f01991caf78dfae9527
Ondřej Češka <ondra.ceska@gmail.com>
2023-05-03 20:23:40 +0200

    pacman view with moves and takes pacman

--- pacman/src/main/java/com/example/pacman/view/PacmanView.java
+++ pacman/src/main/java/com/example/pacman/view/PacmanView.java
@@ -1,6 +1,7 @@
 package com.example.pacman.view;
 
 
+import com.example.pacman.controller.FieldInterface;
 import com.example.pacman.controller.MazeObject;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
@@ -9,8 +10,9 @@ import static javafx.scene.input.KeyCode.*;
 
 public class PacmanView {
 
+    private MazeObject pac = null;
     public PacmanView(MazeObject obj){
-
+        pac = obj;
     }
     public void keyPressed(KeyEvent evt) {
         // Called when the user has pressed a key, which can be
@@ -23,16 +25,17 @@ public class PacmanView {
         KeyCode key = evt.getCode();  // Keyboard code for the pressed key.
 
         if (key == LEFT) {
+            this.pac.move(FieldInterface.Direction.L);
             System.out.println("GfG1");
         }
         else if (key == RIGHT) {
-
+            this.pac.move(FieldInterface.Direction.R);
         }
         else if (key == UP) {
-
+            this.pac.move(FieldInterface.Direction.U);
         }
         else if (key == DOWN) {
-
+            this.pac.move(FieldInterface.Direction.D);
         }
 
     }  // end keyPressed()
