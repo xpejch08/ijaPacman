466fabbc2b56a0ea854786a5c727526b0ebbdf3b
xpejch08 <pejcharstepan@gmail.com>
2023-05-04 22:36:20 +0200

    addingKeyView

---
+++ pacman/src/main/java/com/example/pacman/view/Key.java
@@ -0,0 +1,59 @@
+package com.example.pacman.view;
+
+import com.example.pacman.controller.FieldInterface;
+import com.example.pacman.controller.MazeObject;
+import com.example.pacman.controller.Maze;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.Group;
+import javafx.util.Duration;
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Random;
+
+
+public class Key {
+    private MazeObject pac = null;
+    public Group thisgroup;
+
+    GraphicsContext gc;
+    int CELL_SIZE = 50;
+
+    Canvas canvas = new Canvas();
+    Maze maze;
+    FieldInterface.Direction dir;
+
+    public Key(Maze maze,MazeObject obj, Scene scene, Group group, int rows, int cols){
+        pac = obj;
+        thisgroup = group;
+        this.maze = maze;
+        canvas.setHeight(rows * CELL_SIZE);
+        canvas.setWidth(cols * CELL_SIZE);
+        canvas.setStyle("-fx-border-width: 0;");
+        gc = canvas.getGraphicsContext2D();
+
+        File inputstream = new File("src/images/key.png");
+        Image image = new Image("file:src/images/key.png");
+
+        gc.drawImage(image,50,50);
+        thisgroup.getChildren().add(canvas);
+        this.paint(obj);
+    }
+
+    public void paint(MazeObject obj) {
+        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+        gc.fillRect(obj.cols * CELL_SIZE + 10, obj.rows * CELL_SIZE + 3, CELL_SIZE, CELL_SIZE);
+        Image image = new Image("file:src/images/ghost.png");
+        gc.drawImage(image,50,50);
+    }
+
+}
